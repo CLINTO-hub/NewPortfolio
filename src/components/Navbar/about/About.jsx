14a8240b480c449f544d4d3735d222ca7653288a
CLINTO-hub <clintogeorge007@gmail.com>
2024-04-20 14:25:26 +0530

    latest

--- src/components/Navbar/about/About.jsx
+++ src/components/Navbar/about/About.jsx
@@ -22,12 +22,13 @@ const About = () => {
                 <div className='about-skills'>
                     <div className='about-skill'><p>HTML & CSS</p><hr style={{width:"50%"}}/></div>
                     <div className='about-skill'><p>Javascript</p><hr style={{width:"70%"}}/></div>
-                    <div className='about-skill'><p>Mongo DB</p><hr style={{width:"60%"}}/></div>
-                    <div className='about-skill'><p>React JS</p><hr style={{width:"50%"}}/></div>
-                    <div className='about-skill'><p>Node JS</p><hr style={{width:"60%"}}/></div>
-                    <div className='about-skill'><p>Express JS</p><hr style={{width:"60%"}}/></div>
-                    <div className='about-skill'><p>Tailwind CSS</p><hr style={{width:"60%"}}/></div>
+                    <div className='about-skill'><p>Mongo DB</p><hr style={{width:"80%"}}/></div>
+                    <div className='about-skill'><p>React JS</p><hr style={{width:"80%"}}/></div>
+                    <div className='about-skill'><p>Node JS</p><hr style={{width:"80%"}}/></div>
+                    <div className='about-skill'><p>Express JS</p><hr style={{width:"80%"}}/></div>
+                    <div className='about-skill'><p>Tailwind CSS</p><hr style={{width:"70%"}}/></div>
                     <div className='about-skill'><p>Redux</p><hr style={{width:"60%"}}/></div>
+                    <div className='about-skill'><p>Socket.IO</p><hr style={{width:"60%"}}/></div>
                 </div>
             </div>
         </div>
